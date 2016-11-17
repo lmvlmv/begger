59073931ec502b1d55192b4c389c543a36f574ba
Leon Verrall <lverrall@comp571.openstack.example>
2016-11-17 11:52:58 +0000

    First commit

--- test.py
+++ test.py
@@ -2,19 +2,19 @@
 import beggarmypython
 
 print "Paulhus 1999:"
-beggarmypython.play(('------------KAQ----J------','-JQQK---K----JK--QA-A-JA--'),verbose=False)
+beggarmypython.play(('------------KAQ----J------','-JQQK---K----JK--QA-A-JA--'),verbose=True)
 
 print "Kleber 1999:"
-beggarmypython.play(('---JQ---K-A----A-J-K---QK-','-J-----------AJQA----K---Q'),verbose=False)
+beggarmypython.play(('---JQ---K-A----A-J-K---QK-','-J-----------AJQA----K---Q'),verbose=True)
 
 print "Collins 2006:"
-beggarmypython.play(('A-QK------Q----KA-----J---','-JAK----A--Q----J---QJ--K-'),verbose=False)
+beggarmypython.play(('A-QK------Q----KA-----J---','-JAK----A--Q----J---QJ--K-'),verbose=True)
 
 print "Mann and Wu 2007:"
-beggarmypython.play(('K-KK----K-A-----JAA--Q--J-','---Q---Q-J-----J------AQ--'),verbose=False)
+beggarmypython.play(('K-KK----K-A-----JAA--Q--J-','---Q---Q-J-----J------AQ--'),verbose=True)
 
 print "Nessler 2012:"
-beggarmypython.play(('----Q------A--K--A-A--QJK-','-Q--J--J---QK---K----JA---'),verbose=False)
+beggarmypython.play(('----Q------A--K--A-A--QJK-','-Q--J--J---QK---K----JA---'),verbose=True)
 
 print "Anderson 2013:"
-beggarmypython.play(('--A-Q--J--J---Q--AJ-K---K-','-J-------Q------A--A--QKK-'),verbose=False)
+beggarmypython.play(('--A-Q--J--J---Q--AJ-K---K-','-J-------Q------A--A--QKK-'),verbose=True)
