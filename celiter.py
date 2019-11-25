a702f1196fbf0cec2e9eedd8d3dd977e884677f3
Leon Verrall <lverrall@slb.com>
2019-11-25 22:03:04 +0000

    docker fiddling

--- celiter.py
+++ celiter.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
+from iteration_utilities import grouper
+from beggarplay import play as celplay
+from celery import Celery
 from beggar import BeggarGame, Court, Deck
 import time
-import sys,os
+import sys
+import os
 from tqdm import tqdm
 
 
@@ -12,7 +16,7 @@ def report(res):
 
 start = 4433230883192896
 
-start = 0 
+# start = 0
 deck = Deck(court=Court().default(), decksize=52)
 
 # c = Court()
@@ -24,30 +28,56 @@ deck = Deck(court=Court().default(), decksize=52)
 bg = BeggarGame(deck, gamenum=start)
 
 os.environ['BROKER_POOL_LIMIT'] = 'None'
-from celery import Celery
-from beggarplay import play as celplay
-app = Celery('celiter', backend='rpc://', broker='pyamqp://')
+app = Celery('celiter', backend='rpc://', broker=os.environ['BROKERURL'])
 
-turns = 0
+
 longest = 0
 
-from iteration_utilities import grouper
+handchunk = 10000
+taskchunk = 1000
+rescollect = 100
 
-b = grouper(bg.dealer(), 100000)
-
+b = grouper(bg.dealer(), handchunk)
+res = []
+results = []
 try:
     while True:
-        for chunk in tqdm(iterable=b, total=deck.court.permutations//100000, unit='chunks'):
-            res = celplay.chunks([ (deck.court.courtmap, x) for x in chunk ], 100)()
-            res.get()
+        pbar = tqdm(total=deck.court.permutations,
+                    unit=' hands',
+                    ncols=140,
+                    bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}] {rate_fmt} {postfix[0]} {postfix[1][maxturns]}',
+                    postfix=["Max Turns:", dict(maxturns=0)])
+        for chunk in b:
+            res.append(celplay.chunks(
+                [(deck.court.courtmap, x) for x in chunk], taskchunk)())
+            if len(res) == rescollect:
+                for r in res:
+                    for x in r.results:
+                        for f in x.get():
+                            results.append(f)
+                chunkmax = max([t[0] for t in results])
+                if chunkmax > longest:
+                    longest = chunkmax
+                    pbar.postfix[1]['maxturns'] = longest
+                res = []
+                pbar.update(handchunk*rescollect)
 except StopIteration:
-    pass
+    for r in res:
+        print("Getting result {}".format(r))
+        for x in r.results:
+            for f in x.get():
+                results.append(f)
+    print("Games: {}, Max Turns: {}".format(
+        len(results), max([t[0] for t in results])))
+except KeyboardInterrupt:
+    for r in res:
+        r.forget()
 
 
 # pbar = tqdm(total=deck.court.permutations, unit='Games', ncols=140,
 #         bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}] {rate_fmt} {postfix[0]} {postfix[1][maxturns]}', postfix=["Max Turns:", dict(maxturns=0)])
 # try:
-#     while True:  
+#     while True:
 #         res = celplay.delay(deck.court.courtmap, bg.deal())
 #         (turns, tricks, starts) = res.get()
 #         if turns > longest:
